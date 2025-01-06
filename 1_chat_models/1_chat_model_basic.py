65551177ea77f4f9970b7b64f4815c4c7fcd2e7b
Sukrit Chanachaimongkonkun <tonwan.sukrit@gmail.com>
2025-01-06 23:21:27 +0700

    initial commit

--- 1_chat_models/1_chat_model_basic.py
+++ 1_chat_models/1_chat_model_basic.py
@@ -3,12 +3,19 @@
 
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
+import openai
+import os
 
 # Load environment variables from .env
-load_dotenv()
+load_dotenv(override=True)
+
+# Set OpenAI API key
+# openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# print(os.getenv("OPENAI_API_KEY") , " 1231231231231123132 ")
 
 # Create a ChatOpenAI model
-model = ChatOpenAI(model="gpt-4o")
+model = ChatOpenAI(model="gpt-4o-mini")
 
 # Invoke the model with a message
 result = model.invoke("What is 81 divided by 9?")
