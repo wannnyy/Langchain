ba75dd4995d4f958fa5c0a1bc0be7945442b0d80
Sukrit Chanachaimongkonkun <tonwan.sukrit@gmail.com>
2025-01-18 00:12:33 +0700

    change model

--- 1_chat_models/2_chat_model_basic_conversation.py
+++ 1_chat_models/2_chat_model_basic_conversation.py
@@ -3,10 +3,10 @@ from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 from langchain_openai import ChatOpenAI
 
 # Load environment variables from .env
-load_dotenv()
+load_dotenv(override=True)
 
 # Create a ChatOpenAI model
-model = ChatOpenAI(model="gpt-4o")
+model = ChatOpenAI(model="gpt-4o-mini")
 
 # SystemMessage:
 #   Message for priming AI behavior, usually passed in as the first of a sequenc of input messages.
